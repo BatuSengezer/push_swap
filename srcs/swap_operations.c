cccdcc9dc50dae5ffcf21f7209b8b67c11ce4e93
BatuSengezer <bsengezer@ku.edu.tr>
2023-06-08 18:06:17 +0200

    fixed functions

--- srcs/swap_operations.c
+++ srcs/swap_operations.c
@@ -1,19 +1,32 @@
 #include "../includes/push_swap.h"
 
+
 // Swap the first 2 elements at the top of stack a.
 // Do nothing if there is only one or no elements
-void swap_a(t_stack **stack_a)
+void swap(t_stack **stack)
 {
     t_stack *tmp;
+    t_stack *current;
+    int pos;
 
-    if(!*stack_a || !(*stack_a)->next)
+    if(!*stack || !(*stack)->next)
         return ;
-    tmp->val = (*stack_a)->val;
-    (*stack_a)->val = (*stack_a)->next->val;
-    (*stack_a)->next->val = tmp->val;
-    tmp->sorted_pos = (*stack_a)->sorted_pos;
-    (*stack_a)->sorted_pos = (*stack_a)->next->sorted_pos;
-    (*stack_a)->next->sorted_pos = tmp->sorted_pos;
+    tmp = *stack;
+    *stack = (*stack)->next;
+    tmp->next = (*stack)->next;
+    (*stack)->next = tmp;
+    pos=1;
+    current = *stack;
+    while (current)
+    {
+        current->pos = pos++;
+        current = current->next;
+    }
+}
+
+void swap_a(t_stack **stack_a)
+{
+    swap(stack_a);
     write(1, "sa\n", 3);
 }
 
@@ -21,39 +34,15 @@ void swap_a(t_stack **stack_a)
 // Do nothing if there is only one or no elements.
 void swap_b(t_stack **stack_b)
 {
-    t_stack *tmp;
-
-    if(!*stack_b || !(*stack_b)->next)
-        return ;
-    tmp->val = (*stack_b)->val;
-    (*stack_b)->val = (*stack_b)->next->val;
-    (*stack_b)->next->val = tmp->val;
-    tmp->sorted_pos = (*stack_b)->sorted_pos;
-    (*stack_b)->sorted_pos = (*stack_b)->next->sorted_pos;
-    (*stack_b)->next->sorted_pos = tmp->sorted_pos;
+    swap(stack_b);
     write(1, "sb\n", 3);
 }
 
 // sa and sb at the same time
 void swap_ab(t_stack **stack_a, t_stack **stack_b)
 {
-    t_stack *tmp_a;
-    t_stack *tmp_b;
-
-    if(!*stack_a || !(*stack_a)->next || !*stack_b || !(*stack_b)->next)
-        return ;
-    tmp_a->val = (*stack_a)->val;
-    (*stack_a)->val = (*stack_a)->next->val;
-    (*stack_a)->next->val = tmp_a->val;
-    tmp_a->sorted_pos = (*stack_a)->sorted_pos;
-    (*stack_a)->sorted_pos = (*stack_a)->next->sorted_pos;
-    (*stack_a)->next->sorted_pos = tmp_a->sorted_pos;
-    tmp_b->val = (*stack_b)->val;
-    (*stack_b)->val = (*stack_b)->next->val;
-    (*stack_b)->next->val = tmp_b->val;
-    tmp_b->sorted_pos = (*stack_b)->sorted_pos;
-    (*stack_b)->sorted_pos = (*stack_b)->next->sorted_pos;
-    (*stack_b)->next->sorted_pos = tmp_b->sorted_pos;
+    swap(stack_a);
+    swap(stack_b);
     write(1, "ss\n", 3);
 }
 
