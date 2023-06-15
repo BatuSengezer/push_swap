1e6068db0b42f0ddcb1264821f2878e9ebb6aed5
BatuSengezer <bsengezer@ku.edu.tr>
2023-06-15 06:07:07 +0200

    all done

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: bsengeze <bsengeze@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 23:32:53 by bsengeze          #+#    #+#             */
-/*   Updated: 2023/06/15 05:27:13 by bsengeze         ###   ########.fr       */
+/*   Updated: 2023/06/15 05:59:14 by bsengeze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	main(int ac, char **av)
 	if (ac < 2)
 		return (0);
 	if (!is_correct_input(av)) 
-		exit_error(NULL, NULL);
+		write_error();
 	stack_b = NULL;
 	stack_a = fill_stack_a(ac, av);
 	push_swap(&stack_a, &stack_b);
